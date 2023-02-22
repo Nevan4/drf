d3309a85c7838e884f6a27b264c28c9839ec0a08
Nevan4 <katspru45@gmai.com>
2023-02-22 21:55:11 +0100

    Updated basic.py; views.py urls.py and urls.py in cfehome

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
-# Create your views here.
+
+def api_home(request, *args, **kwargs):
+    # request -> HttpRequest -> Django
+    body = request.body  # byte string of JSON data
+    data = {}
+    try:
+        data = json.loads(body)  # string of JSON data -> Python dict
+    except:
+        pass
+    print(data.keys())
+    # data['headers'] = request.headers
+    print(request.headers)
+    data['content_type'] = request.content_type
+    # return JsonResponse({"message": "This is a Django response"})
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,8 +2,11 @@ import requests
 
 # endpoint = "https://httpbin.org/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={'query': 'Hello'})
-print(get_response.text)
+get_response = requests.get(endpoint, params={'abc': 123}, json={'query': 'Hello'})
+# print("raw text: " + get_response.text)
+# print(get_response.status_code)
+print(get_response.json())
+
 
