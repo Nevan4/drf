7e9c4a41620950b5b280f27e348a0beb64c63a1f
Nevan4 <katspru45@gmai.com>
2023-04-04 22:43:12 +0200

    Small changes in basic.py to test the operability with backend structure Add some records to DB Create forms.py with basic template to show how to pattern looks like Create models.py with class Product and its methods Create serializers.py with ProducSerializer Add new apps in settings Finish views.py with a POST method that can user serializer and print its instace

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,36 +1,72 @@
-from django.http import JsonResponse
 import json
+from django.forms.models import model_to_dict
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
+# changing old api view into new (django rest framework view):
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
-    data = {}
-    if model_data:
-        data['title'] = model_data.title
-        data['content'] = model_data.content
-        data['price'] = model_data.price
-    return JsonResponse(data)
+    """
+    DRF API View
+    """
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        instance = serializer.save()
+        print(instance)
+        return Response(serializer.data)
+
+        # print(serializer.data)
+        # data = serializer.data
+        # return Response(data)
 
 
-
-
-
-#Some bullshit beggining demo
-    # request -> HttpRequest -> Django
-    # print(request.GET)
-    # body = request.body  # byte string of JSON data
+    #
+    # instance = Product.objects.all().order_by("?").first()
     # data = {}
-    # try:
-    #     data = json.loads(body)  # string of JSON data -> Python dict
-    # except:
-    #     pass
-    # print(data.keys())
-    # data['params'] = dict(request.GET)
-    # data['headers'] = dict(request.headers)
-    # print(request.headers)
-    # data['content_type'] = request.content_type
-    # print(request.content_type)
-    # # return JsonResponse({"message": "This is a Django response"})
-    # return JsonResponse(data)
+    # if instance:
+    #     # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
+    #     data = ProductSerializer(instance).data
+    # # return response with django REST framework display
+    # return Response(data)
+    # # return only json to display
+    # # return JsonResponse(data)
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+#     data = {}
+#     if model_data:
+#         #by using model_to_dict all data collected from model_data can be parsed to dict
+#         data = model_to_dict(model_data)
+#         # also only specific params can be picked:
+#         # data = model_to_dict(model_data, fields=['id', 'title'])
+#         # ---
+#         # data['id'] = model_data.id
+#         # data['title'] = model_data.title
+#         # data['content'] = model_data.content
+#         # data['price'] = model_data.price
+#     return JsonResponse(data)
+
+
+# Some bullshit beggining demo
+# request -> HttpRequest -> Django
+# print(request.GET)
+# body = request.body  # byte string of JSON data
+# data = {}
+# try:
+#     data = json.loads(body)  # string of JSON data -> Python dict
+# except:
+#     pass
+# print(data.keys())
+# data['params'] = dict(request.GET)
+# data['headers'] = dict(request.headers)
+# print(request.headers)
+# data['content_type'] = request.content_type
+# print(request.content_type)
+# # return JsonResponse({"message": "This is a Django response"})
+# return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={'abc': 123}, json={'query': 'Hello'})
+get_response = requests.post(endpoint, json={'title': 'Hello Marian'})
 # print("raw text: " + get_response.text)
 # print(get_response.status_code)
 print(get_response.json())
