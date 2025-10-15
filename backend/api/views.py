216b92e87bbf7f9517bf7b33749597c20f160306
Nevan4 <katpsru45@gmail.com>
2025-10-15 19:48:04 +0200

    Trying Response and RF View

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,19 @@
 import json
 from django.forms.models import model_to_dict
-from django.http import JsonResponse, HttpResponse
+# from django.http import JsonResponse, HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
-
-# JsonResponse automatically handles data type serialization (like Decimal to string)
-# while HttpResponse would require manual JSON encoding and type conversion
+    return Response(data)
